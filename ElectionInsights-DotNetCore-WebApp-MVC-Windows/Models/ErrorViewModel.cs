ee7e2574381d8fd87680032da239c105ba4cd0d5
Autumn Sprout <jotasprout@gmail.com>
2022-08-29 00:43:06 -0400

    first commit

---
+++ ElectionInsights-DotNetCore-WebApp-MVC-Windows/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElectionInsights_DotNetCore_WebApp_MVC_Windows.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
